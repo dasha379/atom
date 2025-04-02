f95af1af1f6776e5c051d06d0e42db2c3e916230
dasha379 <hjfktd@yandex.ru>
2025-04-02 21:29:23 +0300

    making let-expression working

--- src/generation.hpp
+++ src/generation.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unordered_map>
 #include "parser.hpp"
 
 class Generator{
@@ -12,11 +13,18 @@ public:
             Generator* gen;
 
             void operator() (const nodeExprIdent& expr_ident){
-
+                if (!gen->m_vars.contains(expr_ident.ident.value.value())){
+                    std::cerr << "undefined identifier: " << expr_ident.ident.value.value() << '\n';
+                    exit(EXIT_FAILURE);
+                }
+                const auto& var = gen->m_vars.at(expr_ident.ident.value.value());
+                std::stringstream offset;
+                offset << "QWORD [rsp + " << (gen->m_stack_size - var.stack_loc - 1) * 8 << "]\n";
+                gen->push(offset.str());
             }
             void operator() (const nodeExprIntLit& expr_int_lit) const {
                 gen->m_output << "    mov rax, " << expr_int_lit.int_lit.value.value() << '\n';
-                gen->m_output << "    push rax\n";
+                gen->push("rax");
             }
         };
         ExprVisitor visitor{.gen = this};
@@ -30,11 +38,16 @@ public:
             void operator() (const nodeStmtExit& stmt_exit) const {
                 gen->gen_expr(stmt_exit.expr);
                 gen->m_output << "    mov rax, 60\n";
-                gen->m_output << "    pop rdi\n";
+                gen->pop("rdi");
                 gen->m_output << "    syscall\n";
             }
             void operator() (const nodeStmtLet& stmt_let){
-
+                if (gen->m_vars.contains(stmt_let.ident.value.value())) {
+                    std::cerr << "identifier already used: " << stmt_let.ident.value.value() << '\n';
+                    exit(EXIT_FAILURE);
+                }
+                gen->m_vars.insert({stmt_let.ident.value.value(), Var {.stack_loc = gen->m_stack_size}});
+                gen->gen_expr(stmt_let.expr);
             }
         };
 
@@ -56,6 +69,22 @@ public:
         return m_output.str();
     }
 private:
+    void push(const std::string& reg){
+        m_output << "    push " << reg << '\n';
+        m_stack_size++;
+    }
+
+    void pop(const std::string& reg){
+        m_output << "    pop " << reg << '\n';
+        m_stack_size--;
+    }
+
+    struct Var{
+        size_t stack_loc;
+    };
+
     const nodeProg m_prog;
     std::stringstream m_output;
+    size_t m_stack_size = 0;
+    std::unordered_map<std::string, Var> m_vars;
 };
